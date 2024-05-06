ff130709fb4b4f71683260b879841cb396fba698
Caue Garcia <cauerg7@outlook.com>
2024-05-06 17:32:07 -0300

    full search request - posts.

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.workshopmongo.controllers;
 
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -34,16 +36,9 @@ public class PostController {
 		text = URL.decodeParam(text);
 		return service.findByTitle(text);
 	}
-	/*
-	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
-		text = URL.decodeParam(text);
-		List<PostDTO> list = service.findByTitle(text);
-		return ResponseEntity.ok(list);
-	}
 	
 	@GetMapping(value = "/fullsearch")
-	public ResponseEntity<List<PostDTO>> fullSearch(
+	public Flux<PostDTO> fullSearch(
 			@RequestParam(value = "text", defaultValue = "") String text,
 			@RequestParam(value = "minDate", defaultValue = "") String minDate,
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException, ParseException {
@@ -52,8 +47,6 @@ public class PostController {
 		Instant min = URL.convertDate(minDate, Instant.EPOCH);
 		Instant max = URL.convertDate(maxDate, Instant.now());
 		
-		List<PostDTO> list = service.fullSearch(text, min, max);
-		return ResponseEntity.ok(list);
+		return service.fullSearch(text, min, max);
 	}
-	*/
 }
